269772aad9105c13fdd9fb8de5998fcfd87beb27
sankalp <tripathisachin286@gmail.com>
2026-04-27 06:11:32 +0530

    fix: restore App.jsx

---
+++ my-group-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <p>hello guys 👋 (edited by me)</p>
+      <p>lets start our project</p>
+    </div>
+  )
+}
+
+export default App
